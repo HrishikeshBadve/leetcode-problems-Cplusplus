e5ab6f0aee496939cdbbc909a2c66c589f5b319a
Hrishikesh Badve <112002709+HrishikeshBadve@users.noreply.github.com>
2024-03-11 20:36:45 -0500

    Create Best Time to Buy and Sell Stock II.cpp

---
+++ Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int valley=prices[0];
+        int peak=prices[0];
+        int maxProfit=0;
+        int i=0;
+        while(i<(prices.size()-1))
+        {
+            while(i<(prices.size()-1) && prices[i]>=prices[i+1])
+            {
+                i++;
+            }
+            valley = prices[i];
+            while(i<(prices.size()-1) && prices[i]<=prices[i+1])
+            {
+                i++;
+            }
+            peak = prices[i];
+            maxProfit += peak-valley;
+        }      
+        return maxProfit;
+    }
+};
