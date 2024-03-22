87efc0cfb8d8670f679a90fe7d680b0667efcd8a
Hrishikesh Badve <112002709+HrishikeshBadve@users.noreply.github.com>
2024-03-22 16:11:53 -0500

    Update Delete Node in a Linked List.cpp

--- Delete Node in a Linked List.cpp
+++ Delete Node in a Linked List.cpp
@@ -14,7 +14,7 @@ Output: [4,1,9]
 Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling your function.
 */
 
-/**
+/*
 Definition for singly-linked list.
 struct ListNode {
     int val;
